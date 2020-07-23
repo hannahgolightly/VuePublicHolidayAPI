56ec3e0b6dc9ff5a111cae77bb5e7f611584983e
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-07-23 17:01:36 +0100

    removing commented out code snippets

--- src/components/NextHolsPanel.vue
+++ src/components/NextHolsPanel.vue
@@ -35,20 +35,6 @@ export default {
       })
   }
 }
-
-// axios
-//   .get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p1')
-//   .then(response => {
-//     this.setState({ p1Location: response.data });
-//     return axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p2');
-//   })
-//   .then(response => {
-//     this.setState({ p2Location: response.data });
-//     return axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p3');
-//   })
-//   .then(response => {
-//     this.setState({ p3Location: response.data });
-//   }).catch(error => console.log(error.response));
 </script>
 
 <style lang="css" scoped>
